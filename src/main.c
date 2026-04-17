9731ff5a261396bdb117b822d955e77daa5ae400
Henry Unite <unitehenry@gmail.com>
2026-04-17 05:25:48 +0000

    feat: init raylib hello world

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <emscripten/emscripten.h>
+
+void draw(void) {
+  BeginDrawing();
+
+  ClearBackground(RAYWHITE);
+
+  DrawText(TextFormat("Hello World"), 190, 200, 20, DARKGRAY);
+
+  EndDrawing();
+}
 
 int main() {
-  printf("Hello World\n");
+  static const char *title = "rayflap";
+  static const int screenWidth = 480;
+  static const int screenHeight = 720;
+
+  InitWindow(screenWidth, screenHeight, title);
+
+  emscripten_set_main_loop(draw, 60, 1);
 
   return 0;
 }
