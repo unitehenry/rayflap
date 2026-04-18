ca3569eee067acdda760152267d41fe14927e333
Henry Unite <unitehenry@gmail.com>
2026-04-18 05:11:51 +0000

    feat: draw message

--- src/main.c
+++ src/main.c
@@ -5,7 +5,19 @@
 #define MainLoop emscripten_set_main_loop
 
 Texture2D background_texture;
+Texture2D message_texture;
 int scrollX = 0;
+
+void draw_message() {
+  if (!IsTextureValid(message_texture)) {
+    message_texture = LoadTexture("assets/sprites/message.png");
+  }
+
+  float centerX = (288 - message_texture.width) / 2.0f;
+  float centerY = (512 - message_texture.height) / 2.0f;
+
+  DrawTexture(message_texture, centerX, centerY, WHITE);
+}
 
 void draw_background() {
   if (!IsTextureValid(background_texture)) {
@@ -24,6 +36,8 @@ void draw(void) {
   BeginDrawing();
 
   draw_background();
+
+  draw_message();
 
   EndDrawing();
 }
